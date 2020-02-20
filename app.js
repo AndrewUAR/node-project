98d475a727a2eb1186390dc84c9d87381831af96
Andrew <andrewriznykua93@gmail.com>
2020-02-20 16:40:53 +0200

    app refactored

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res
-        .status(200)
-        .json({message: 'Hello from the server side!', app: 'Natours'});
+app.use(express.json());  //<---middleware
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+    console.log('Hello from the middleware');
+    next();
 })
 
-app.post('/', (req, res) => {
-    res
-        .status(200)
-        .send('You can post to this endpoint...')
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
 })
-const port = 3000;
-app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
-});
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
