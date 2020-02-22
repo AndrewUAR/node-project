92776469694fb50f4ffb48c4bc740dfd221b3b0e
Andrew <andrewriznykua93@gmail.com>
2020-02-22 23:58:08 +0200

    update and delete methods implemented

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -61,7 +61,7 @@ exports.getTour = async (req, res) => {
 
 exports.createTour = async (req, res) => {
   try {
-    const newTour = await Tour(req.body);
+    const newTour = await Tour.create(req.body);
     res.status(201).json({
       status: 'success',
       data: {
@@ -73,20 +73,38 @@ exports.createTour = async (req, res) => {
       status: 'fail',
       message: err
     });
-    console.log(err);
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: '<Updated tour here...>'
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    res.status(200).json({
+      status: 'success',
+      data: tour
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: null
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
