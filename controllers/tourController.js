8eb975c13ba08d497d09adba34d8317ec3647c74
Andrew <andrewriznykua93@gmail.com>
2020-02-23 21:55:34 +0200

    top tours route

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -24,10 +24,17 @@ const Tour = require('../models/tourModel');
 //   next();
 // };
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
-    // eslint-disable-next-line node/no-unsupported-features/es-syntax
     //BUILD QUERY
+
     // 1) FILTERING
 
     // eslint-disable-next-line node/no-unsupported-features/es-syntax
