7ec6b9d5388007523726f8a949318e39f2bbe3c9
Andrew <andrewriznykua93@gmail.com>
2020-02-23 21:40:56 +0200

    pagination implemented

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -66,6 +66,21 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    //5) PAGINATION
+
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
+    //6) SEND RESPONSE
+
     const tours = await query;
     res.status(200).json({
       status: 'success',
