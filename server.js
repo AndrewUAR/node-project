6498bb735a6f9b4e0c34a9bf94f4bbb1a804871e
Andrew <andrewriznykua93@gmail.com>
2020-02-24 23:59:59 +0200

    operational, non-operational errors, uncaught exception and unhandled rejection handled

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+/* eslint-disable no-console */
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
@@ -19,15 +26,19 @@ mongoose
   .then(() => {
     // eslint-disable-next-line no-console
     console.log('DB connection succesfull');
-  })
-  .catch(err => {
-    // eslint-disable-next-line no-console
-    console.log(err);
   });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
