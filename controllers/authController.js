d705969846e3a5236052d8b72f841d16705539ff
Andrew <andrewriznykua93@gmail.com>
2020-02-26 00:21:50 +0200

    JWT error handling methods

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-console */
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
@@ -58,13 +58,13 @@ exports.protect = catchAsync(async (req, res, next) => {
   ) {
     token = req.headers.authorization.split(' ')[1];
   }
-  console.log('token', token);
   if (!token) {
     return next(
       new AppError('You are not logged in! Please log in to get access.', 401)
     );
   }
   //2) Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
   //3) Check if user still exits
 
   //4) Check if user changed password after JWT was issued
