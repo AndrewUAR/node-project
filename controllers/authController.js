e9871742c3e879f5a501a25bfcfc4a7f2031d807
Andrew <andrewriznykua93@gmail.com>
2020-02-26 01:03:23 +0200

    protect middleware implemented

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,7 +16,8 @@ exports.signup = catchAsync(async (req, res, next) => {
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm
+    passwordConfirm: req.body.passwordConfirm,
+    passwordChangedAt: req.body.passwordChangedAt
   });
 
   const token = await signToken(newUser._id);
@@ -66,7 +67,20 @@ exports.protect = catchAsync(async (req, res, next) => {
   //2) Verification token
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
   //3) Check if user still exits
-
+  const freshUser = await User.findById(decoded.id);
+  if (!freshUser) {
+    return next(
+      new AppError('The user belonging to this token does no longer exists.')
+    );
+  }
   //4) Check if user changed password after JWT was issued
+  if (freshUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User recently changed password! Please log in again.', 401)
+    );
+  }
+
+  //Grant access to protected route
+  req.user = freshUser;
   next();
 });
--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -34,7 +35,8 @@ const userSchema = new mongoose.Schema({
       },
       message: 'Password does not match.'
     }
-  }
+  },
+  passwordChangedAt: Date
 });
 
 userSchema.pre('save', async function(next) {
@@ -52,6 +54,19 @@ userSchema.methods.correctPassword = async function(
   userPassword
 ) {
   return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+    console.log(changedTimestamp, JWTTimestamp);
+    return JWTTimestamp < changedTimestamp;
+  }
+  //False means not changed
+  return false;
 };
 
 const User = mongoose.model('User', userSchema);
