b072e3410622643d66149f9eb515fe6f1dfcf559
Andrew <andrewriznykua93@gmail.com>
2020-02-27 11:19:56 +0200

    cookies added to respond

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,20 @@ const signToken = id => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    // secure: true, //will be send just via https //just in production
+    httpOnly: true //cant be modified by a browser in any way
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  //don't send encrypted password to user
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
     token,
