2ed306d2087b78b21245b0e6fb7945d332808cb7
Andrew <andrewriznykua93@gmail.com>
2020-03-02 12:31:41 -0600

    getAllReviews, getReview, createReview replaced with handleFactory

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,21 +1,19 @@
 const Review = require('../models/reviewModel');
-// const APIFeatures = require('../utils/apiFeatures');
-const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
 const factory = require('./handlerFactory');
 
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-  let filter = {};
-  if (req.params.tourId) filter = { tour: req.params.tourId };
-  const reviews = await Review.find(filter);
-  res.status(200).json({
-    status: 'success',
-    results: reviews.length,
-    data: {
-      reviews
-    }
-  });
-});
+exports.getAllReviews = factory.getAll(Review);
+// exports.getAllReviews = catchAsync(async (req, res, next) => {
+//   let filter = {};
+//   if (req.params.tourId) filter = { tour: req.params.tourId };
+//   const reviews = await Review.find(filter);
+//   res.status(200).json({
+//     status: 'success',
+//     results: reviews.length,
+//     data: {
+//       reviews
+//     }
+//   });
+// });
 
 // exports.createReview = catchAsync(async (req, res, next) => {
 //   //Allow nested routes
@@ -31,18 +29,19 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 //   });
 // });
 
-exports.getReview = catchAsync(async (req, res, next) => {
-  const review = await Review.findById(req.params.id);
-  if (!review) {
-    return next(new AppError('No review found with that ID', 404));
-  }
-  res.status(200).json({
-    status: 'success',
-    data: {
-      review
-    }
-  });
-});
+exports.getReview = factory.getOne(Review);
+// exports.getReview = catchAsync(async (req, res, next) => {
+//   const review = await Review.findById(req.params.id);
+//   if (!review) {
+//     return next(new AppError('No review found with that ID', 404));
+//   }
+//   res.status(200).json({
+//     status: 'success',
+//     data: {
+//       review
+//     }
+//   });
+// });
 
 exports.setTourUserIds = (req, res, next) => {
   if (!req.body.tour) req.body.tour = req.params.tourId;
