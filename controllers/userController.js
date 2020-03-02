dc7d2e106eac150229ebb9cb10fb79bde6331f4c
Andrew <andrewriznykua93@gmail.com>
2020-03-02 12:33:59 -0600

    getAllUsers, getUser replaced with handlerFactory

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,16 +13,17 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find();
-  res.status(200).json({
-    status: 'success',
-    results: users.length,
-    data: {
-      users
-    }
-  });
-});
+exports.getAllUsers = factory.getAll(User);
+// exports.getAllUsers = catchAsync(async (req, res, next) => {
+//   const users = await User.find();
+//   res.status(200).json({
+//     status: 'success',
+//     results: users.length,
+//     data: {
+//       users
+//     }
+//   });
+// });
 
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if user POSTs password data
@@ -59,18 +60,19 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getUser = catchAsync(async (req, res, next) => {
-  // const user = await User.findById(req.params.id);
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
-  });
-});
+exports.getUser = factory.getOne(User);
+// exports.getUser = catchAsync(async (req, res, next) => {
+//   // const user = await User.findById(req.params.id);
+//   res.status(500).json({
+//     status: 'error',
+//     message: 'This route is not yet defined!'
+//   });
+// });
 
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet defined!'
+    message: 'This route is not defined! Please use /signup instead.'
   });
 };
 
