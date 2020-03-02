b8188332572135fbf095f2f3a1affc62ad7dbe70
Andrew <andrewriznykua93@gmail.com>
2020-03-01 22:02:05 -0600

    handlerFactory create, update

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -17,19 +17,19 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.createReview = catchAsync(async (req, res, next) => {
-  //Allow nested routes
-  if (!req.body.tour) req.body.tour = req.params.tourId;
-  if (!req.body.user) req.body.user = req.user.id;
+// exports.createReview = catchAsync(async (req, res, next) => {
+//   //Allow nested routes
+//   if (!req.body.tour) req.body.tour = req.params.tourId;
+//   if (!req.body.user) req.body.user = req.user.id;
 
-  const newReview = await Review.create(req.body);
-  res.status(201).json({
-    status: 'success',
-    data: {
-      tour: newReview
-    }
-  });
-});
+//   const newReview = await Review.create(req.body);
+//   res.status(201).json({
+//     status: 'success',
+//     data: {
+//       tour: newReview
+//     }
+//   });
+// });
 
 exports.getReview = catchAsync(async (req, res, next) => {
   const review = await Review.findById(req.params.id);
@@ -44,4 +44,11 @@ exports.getReview = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.setTourUserIds = (req, res, next) => {
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+  next();
+};
+exports.createReview = factory.updateOne(Review);
+exports.updateReview = factory.updateOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
