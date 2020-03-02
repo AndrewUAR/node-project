443725eb49c34d27e6a19bb51282dcc00b365e23
Andrew <andrewriznykua93@gmail.com>
2020-03-01 22:00:29 -0600

    patch route added

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -10,10 +10,14 @@ router
   .post(
     authController.protect,
     authController.restrictTo('user'),
+    reviewController.setTourUserIds,
     reviewController.createReview
   );
 
-router.route('/:id').delete(reviewController.deleteReview);
-router.route('/:id').get(reviewController.getReview);
+router
+  .route('/:id')
+  .patch(reviewController.updateReview)
+  .delete(reviewController.deleteReview)
+  .get(reviewController.getReview);
 
 module.exports = router;
