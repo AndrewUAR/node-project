1a0e82ba01dbb51c2b9495e89c871f173afb9a26
Andrew <andrewriznykua93@gmail.com>
2020-03-02 19:16:57 -0600

    catchAsync import deleted

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -45,9 +45,9 @@ exports.getReview = factory.getOne(Review);
 
 exports.setTourUserIds = (req, res, next) => {
   if (!req.body.tour) req.body.tour = req.params.tourId;
-  if (!req.body.user) req.body.user = req.user.id;
+  req.body.user = req.user.id;
   next();
 };
-exports.createReview = factory.updateOne(Review);
+exports.createReview = factory.createOne(Review);
 exports.updateReview = factory.updateOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
